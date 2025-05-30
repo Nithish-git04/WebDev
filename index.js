3e89f442422a15fc435ce5fe821ee133ef12bf09
Nithish-git04 <nithishthanigaivel.2005@gmail.com>
2025-05-30 13:45:41 +0530

    Version2

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-function Person(fname, lname, age) {
-    this.first_name = fname;
-    this.last_name = lname;
-    this.age = age;
-    this.full_name = function(){
-        return this.first_name + " " + this.last_name;
+window.alert("Hello from JS!");
+
+function submitted() {
+    document.getElementById("feedback_form").onsubmit = function() {
+        alert("Thank you for your feedback!");
+        return false;
     }
 }
-
-Person.prototype.language = "English";
-
-var myself = new Person("John", "Doe", 30);
-
-
-Person.prototype.change_name = function(newFname, newLname) {
-    this.first_name = newFname;
-    this.last_name = newLname;
-};
-
-let myslef1 = new Person("Jane", "Doe", 25);
-myslef1.change_name("Janet", "Smith");
-
-// console.log(myself);
